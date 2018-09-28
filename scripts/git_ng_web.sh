fc99f6ee064023351f3fe57161b267087bb776e8
Aurélien Matouillot <a.matouillot@gmail.com>
2018-09-28 18:23:55 +0200

    Improve uwsgi runner

--- scripts/git_ng_web.sh
+++ scripts/git_ng_web.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env sh
 
-source env/bin/activate
+SCRIPT_PATH=$( cd "$( dirname "$0" )" && pwd )
+LOG_FILE="$SCRIPT_PATH/uwsgi.log"
+PID_FILE="$SCRIPT_PATH/uwsgi.pid"
+CONF_FILE="$SCRIPT_PATH/production.ini"
+
+
+source "$SCRIPT_PATH/env/bin/activate"
 
 
 case "$1" in
   start)
-    uwsgi --ini-paste production.ini
+    uwsgi --ini-paste $CONF_FILE -d $LOG_FILE --pidfile $PID_FILE
     ;;
   stop)
-    uwsgi --stop uwsgi.pid
+    uwsgi --stop $PID_FILE
     ;;
   restart)
     $0 stop
+    sleep 1
     $0 start
     ;;
   *)
